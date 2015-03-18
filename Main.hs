fec8183d16116d23f0bd5516a891fb0d2ad567f9
Petter Rodhelind <petter.rodhelind@gmail.com>
2015-03-18 19:08:31 +0100

    added support for cmd line source argument

--- Main.hs
+++ Main.hs
@@ -7,8 +7,10 @@
 module Main where
 
 -- | Import
-import System.Directory (doesDirectoryExist, doesFileExist, createDirectory, removeFile,
-                         setCurrentDirectory, getDirectoryContents, removeDirectory, copyFile)
+import System.Directory (doesDirectoryExist, doesFileExist, createDirectory,
+                         removeFile, setCurrentDirectory, getDirectoryContents,
+                         copyFile, getCurrentDirectory)
+import System.Environment (getArgs)
 import Text.Pandoc (writeHtmlString, readMarkdown, def)
 import Text.Regex.PCRE
 import Data.Char (toLower)
@@ -23,13 +25,17 @@ data Page  = Page (Title, Date) String
 
 
 -- | MAIN
+main :: IO ()
 main = do
-       srcExists <- doesDirectoryExist "./source"
+       args <- getArgs
+       baseDir <- getCurrentDirectory
+       let srcDir = if length args == 1 then args!!0 else "./source"
+       srcExists <- doesDirectoryExist srcDir
        if not srcExists
           then putStrLn "Source directory not found."
           else do
                -- get dir content
-               setCurrentDirectory "./source"
+               setCurrentDirectory srcDir
                content <- getDirectoryContents "."
                -- filter out files and then *.md files
                files <- filterM doesFileExist content
@@ -43,13 +49,13 @@ main = do
 
                          -- jump back, get the standard template, create a menu
                          -- and merge into the template
-                         setCurrentDirectory ".."
-                         rawTemplate <- readFile "./source/index.tpl"
+                         setCurrentDirectory baseDir
+                         rawTemplate <- readFile $ srcDir ++ "/index.tpl"
                          let menu = "<ul>\n" ++ (createMenu pages) ++ "</ul>\n"
                          let finalTemplate = replaceTag rawTemplate "[-MENU-]" menu
                          putStrLn "Created menu"
 
-                         -- clear the "./output" dir of old stuff
+                         -- clear the "./html" dir of old stuff
                          clearOutputDir
                          putStrLn "Cleared output"
                          putStrLn "---"
@@ -61,9 +67,9 @@ main = do
                          let nonMdFiles = [f | f <- files,
                                                not (f `elem` markdownFiles),
                                                not (f == "index.tpl")]
-                         mapM (\f -> do
-                                      copyFile ("./source/" ++ f) ("./output/" ++ f)
-                                      putStrLn ("Copied source/" ++ f)
+                         _ <- mapM (\f -> do
+                                      copyFile (srcDir ++ "/" ++ f) ("./html/" ++ f)
+                                      putStrLn ("Copied " ++ srcDir ++ "/" ++ f)
                              ) nonMdFiles
 
                          -- announce finish
@@ -75,11 +81,11 @@ main = do
 
 -- | Write page to output directory via template
 writeOutput :: String -> Page -> IO ()
-writeOutput template (Page (t, d) page) = do
+writeOutput template (Page (t, _) page) = do
             writeFile filename output
             putStrLn ("Created " ++ filename)
             where
-                filename = "output/" ++ (convertTitle t) ++ ".html"
+                filename = "html/" ++ (convertTitle t) ++ ".html"
                 output = replaceTag (replaceTag template "[-TITLE-]" ("| " ++ t)) "[-MAIN-]" page
 
 -- | Create an HTML menu based on loaded pages
@@ -96,15 +102,15 @@ createMenu ((Page (t, _) _):ps) = "<li><a href=\"./" ++
 -- | Clear the output directory of all old files
 clearOutputDir :: IO ()
 clearOutputDir = do
-                   y <- doesDirectoryExist "./output"
+                   y <- doesDirectoryExist "./html"
                    if y
                       then do
-                           setCurrentDirectory "./output"
+                           setCurrentDirectory "./html"
                            content <- getDirectoryContents "."
                            files <- filterM doesFileExist content
                            mapM_ removeFile files
                            setCurrentDirectory ".."
-                      else createDirectory "./output"
+                      else createDirectory "./html"
                              
 
 
--- todo.md
+++ todo.md
@@ -3,4 +3,3 @@
 - small builtin test web server
     - started with cmd line arg
 - meta tags in source file
-- cmd line arg for source dir
